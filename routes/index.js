e972e48d800c4146ebdab7552af83800a1bea4d0
xuni <xuning@ybyt.cc>
2016-11-09 16:45:06 +0800

    添加用户列表页面

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,13 @@ router.get('/newRecord', function(req, res, next) {
   res.render('newRecord',{title:"建立健康档案"});
 });
 
+router.get('/userList', function(req, res, next) {
+  res.render('userList',{title:"患者列表"});
+});
+
+router.get('/fileQuery', function(req, res, next) {
+  res.render('fileQuery',{title:"档案查询"});
+});
+
+
 module.exports = router;
