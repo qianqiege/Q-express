773e2b799b610b0becd120f6cd89921d23cd9dd5
xuni <xuning@ybyt.cc>
2016-11-09 10:05:21 +0800

    添加建档页面

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,12 @@ router.get('/', function(req, res, next) {
 router.get('/test', function(req, res, next) {
   res.render('test',{title:"test"});
 });
+
+router.get('/newRecord', function(req, res, next) {
+  res.render('newRecord',{title:"建立健康档案"});
+});
+
+router.post('http://192.168.1.225:3005/api/v1/getuser/record', function(req, res, next) {
+});
+
 module.exports = router;
