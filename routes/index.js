9d387b2c90897c4cc939dbe220e994a4b22344d3
river <sffddia@163.com>
2016-11-22 17:43:22 +0800

    增加医生列表页面

--- routes/index.js
+++ routes/index.js
@@ -54,6 +54,10 @@ router.get('/userList', function(req, res, next) {
     res.render('userList',{title:"患者列表"});
 });
 
+router.get('/doctorsList', function(req, res, next) {
+    res.render('doctorsList',{title:"医生列表"});
+});
+
 router.get('/fileQuery', function(req, res, next) {
     res.render('fileQuery',{title:"档案查询"});
 });
