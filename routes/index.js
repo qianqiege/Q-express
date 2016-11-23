29adde3d0ffb499371a5a44e530e455c88196a18
river <sffddia@163.com>
2016-11-23 14:12:53 +0800

    处理合并分支解决冲突出现bug,

--- routes/index.js
+++ routes/index.js
@@ -56,6 +56,7 @@ router.get('/userList', function(req, res, next) {
 
 router.get('/doctorsList', function(req, res, next) {
     res.render('doctorsList',{title:"医生列表"});
+});
 
 router.get('/add_friends', function(req, res, next) {
   res.render('add_friends',{title:"新朋友列表"});
